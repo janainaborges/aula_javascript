678c968aa2ad68deb3232621f84d2abcf6e83a32
janainaborges <janainaborgesdr@gmail.com>
2021-03-21 20:38:12 -0300

    definir movimentos da cobra

--- js/main.js
+++ js/main.js
@@ -7,21 +7,43 @@ snake[0] = {
     y: 8 * box
 }
 
+let direction = "right";
 
-function criarBG() {
+function criarBG(){
     context.fillStyle = "lightgreen"
     context.fillRect(0, 0, 16 * box, 16 * box)
 }
 
 
 function criarCobrinha(){
-    for(1=0; i < SVGAnimatedLength.length; i++){
+    for(1=0; i < snake.length; i++){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
 }
 
+function iniciarJogo(){
+
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = [0].x;
+    let snakeY = [0].y;
+
+    if(direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX, 
+        y:snakeY
+    }
+
+    snake.unshift(newHead);
+}
+let jogo =setInterval(iniciarJogo,100);
 
 
-criarBG();
-criarCobrinha;
